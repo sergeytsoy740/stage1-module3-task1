f427d713877aeff9a74d9aeed7e00985ee8f7e7c
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-18 20:06:02 +0600

    Stage-1 Generics task

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -4,23 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Generics {
-
     //TODO: Refactor Method-1
-    public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
-        firstList.add(name);
-        List secondList = new ArrayList<>();
+    public <T> List<List<? super T>> boxingMethod(T t) {
+
+        List<? super T> firstList = new ArrayList<>();
+        firstList.add(t);
+
+        List<List<? super T>> secondList = new ArrayList<>();
         secondList.add(firstList);
+
         return secondList;
     }
 
     //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+    public <T> Object genericMethod(T data) {
         return data;
     }
 
     //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
+    public <T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
